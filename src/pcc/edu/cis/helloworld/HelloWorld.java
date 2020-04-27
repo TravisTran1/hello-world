32bf7724d0e34c4904457a739019a976d94a3419
Travis Tran <travis.tran1@pcc.edu>
2020-04-27 16:00:54 -0700

    added main method that prints hello world

--- src/pcc/edu/cis/helloworld/HelloWorld.java
+++ src/pcc/edu/cis/helloworld/HelloWorld.java
@@ -1,4 +1,12 @@
 package pcc.edu.cis.helloworld;
 
+/**
+ * @author Travis Tran
+ * @version 04/27/2020
+ */
+
 public class HelloWorld {
+    public static void main(String [] args) {
+        System.out.println("Hello World!");
+    }
 }
