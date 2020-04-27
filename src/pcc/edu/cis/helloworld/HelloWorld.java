3bb1eb4cf0f1b9c65374bbbbb2f0d8b9d112f416
Travis Tran <travis.tran1@pcc.edu>
2020-04-27 16:03:37 -0700

    print date

--- src/pcc/edu/cis/helloworld/HelloWorld.java
+++ src/pcc/edu/cis/helloworld/HelloWorld.java
@@ -1,5 +1,7 @@
 package pcc.edu.cis.helloworld;
 
+import java.util.Date;
+
 /**
  * @author Travis Tran
  * @version 04/27/2020
@@ -8,5 +10,6 @@ package pcc.edu.cis.helloworld;
 public class HelloWorld {
     public static void main(String [] args) {
         System.out.println("Hello World!");
+        System.out.println(new Date());
     }
 }
